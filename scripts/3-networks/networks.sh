900bfc87347373b4fe06c151b1d640445a3a6336
adamgft <66415638+adamgft@users.noreply.github.com>
2021-08-19 10:11:52 +0100

    Update networks.sh

--- scripts/3-networks/networks.sh
+++ scripts/3-networks/networks.sh
@@ -65,14 +65,13 @@ COPY_LOCATION=.
 git add .
 git commit -m 'Your message'
 
-echo "----"
-pwd
-ls
-echo "----"
-
 echo Removing unneeded backend example file
 TF_EXAMPLE_VARS=./envs/shared/backend.tf
 [ -f $TF_EXAMPLE_VARS ] && { echo "Removing unneeded $TF_EXAMPLE_VARS file: $TF_EXAMPLE_VARS"; rm $TF_EXAMPLE_VARS; } || { echo "No $TF_EXAMPLE_VARS file found"; exit 1; }
+
+echo "---"
+cat ../../scripts/3-networks/backend.tf
+echo "---"
 
 # echo Copying in needed backend example file
 # TF_VARS=../../scripts/3-networks/backend.tf
@@ -82,11 +81,6 @@ TF_EXAMPLE_VARS=./envs/shared/backend.tf
 echo Local shared file TF apply
 cd ./envs/shared/
 
-echo "----"
-ls 
-pwd
-cat backend.tf
-echo "----"
 
 # terraform init
 # terraform plan
